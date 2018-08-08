756f7b3c7b2833a1be79e79fcaed4c76278f7e27
Philipe Louchtch - de Raadt <philipelouchtch@outlook.com>
2018-08-08 09:43:27 +0200

    add SingletonObjectCache

---
+++ core/src/main/java/louchtch/expensito/louchtch/util/SingletonObjectCache.java
@@ -0,0 +1,34 @@
+package louchtch.expensito.louchtch.util;
+
+import louchtch.expensito.model.Tag;
+
+import java.lang.ref.WeakReference;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+public class SingletonObjectCache<K, V>
+{
+	private Map<K, WeakReference<V>> cache;
+
+	private SingletonObjectCache(Map<K, WeakReference<V>> cache)
+	{
+		this.cache = cache;
+	}
+
+	public V computeIfAbsent(K key, Function<K, V> computeFunction)
+	{
+		//TODO: is there a race condition here between GC and WeakReference#get ??
+
+		Function<K, WeakReference<V>> weakReferenceComputeFunction = (K k) -> new WeakReference<>(computeFunction.apply(k));
+		WeakReference<V> weaklyReferencedValue = cache.computeIfAbsent(key, weakReferenceComputeFunction);
+
+		return weaklyReferencedValue.get();
+	}
+
+	public static <K, V> SingletonObjectCache<K, V> freshInstance()
+	{
+		HashMap<K, WeakReference<V>> map = new HashMap<>();
+		return new SingletonObjectCache<>(map);
+	}
+}
