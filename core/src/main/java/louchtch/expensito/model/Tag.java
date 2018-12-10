49ffd16a8a211d3c49a31f870f38d32693a6f2a7
Philipe Louchtch - de Raadt <philipelouchtch@outlook.com>
2018-12-10 14:20:43 +0100

    fix compilation of Tag class

--- core/src/main/java/louchtch/expensito/model/Tag.java
+++ core/src/main/java/louchtch/expensito/model/Tag.java
@@ -1,5 +1,7 @@
 package louchtch.expensito.model;
 
+import louchtch.util.SingletonObjectCache;
+
 import java.lang.ref.WeakReference;
 import java.util.Map;
 
@@ -7,10 +9,19 @@ public class Tag
 {
 	private String label;
 
+	private Tag(String label)
+	{
+		this.label = label;
+	}
+
 	public String label()
 	{
 		return label;
 	}
 
-	private static Map<String, WeakReference<Tag>>
+	private static SingletonObjectCache<String, Tag> tags;
+	public static Tag from(String label)
+	{
+		return tags.computeIfAbsent(label, Tag::new);
+	}
 }
